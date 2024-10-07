6d702ac9eaf8e62ae2152469f74df12ee3459540
Aryan Taneja <tanejaaryan85@gmail.com>
2024-10-08 00:33:57 +0530

    Function

--- Hello.js
+++ Hello.js
@@ -186,3 +186,33 @@ console.log(slicedString);
 // This extracts from the 6th character from the end to the 1st character from the end.
 
 
+// FUNCTIONS
+// to declare functions in JS we just write function name(variable){function defination}
+// For Example:
+function CalculateSqAndCube(num){
+    console.log("Square: " + (num*num));
+    console.log("Cube: " + (num*num*num));
+}
+
+CalculateSqAndCube(2);
+CalculateSqAndCube(3);
+CalculateSqAndCube(4);
+
+// Anatomy of a function
+// Let's break down the structure of a JavaScript function word by word:
+
+function greet(name) {
+    console.log("Hello, " + name + "!");
+}
+
+greet("Aryan");
+// function: This is the keyword used to declare a function in JavaScript. It tells the interpreter that you are defining a new function.
+
+// greet: This is the name of the function. You can choose any valid identifier as the function name. The name is used to call the function later in your code.
+
+// (name): These are the parameters of the function. Parameters act as placeholders for values that you can pass into the function when you call it. In this case, the greet function takes one parameter, name.
+
+// { }: These curly braces define the code block of the function. The actual instructions or statements to be executed when the function is called are placed inside these braces.
+
+// console.log("Hello, " + name + "!");
+//: This is the body of the function, where the actual code to be executed is written. In this example, the function uses console.log() to print a greeting message to the console, incorporating the value of the name parameter.
